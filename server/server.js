6a918198a3948eb1930131705000f12fd4eff36b
Vinícius de Oliveira <vi.o@hotmail.com>
2019-02-20 12:14:47 -0300

    alteracao do get

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos',(req,res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(400).send();
+  }
+  Todo.findById(id).then((idSearch) => {
+    if(!idSearch){
+      console.log('asdasd');
+      return res.status(400).send();
+    }
+    res.send({idSearch})
+  })
+    
+}, (e) => {
 });
 
 app.listen(3000, () => {
